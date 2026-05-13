9a954adba976ac839f8a1035bd83786a976ea604
DeltaDizzy <EJBraswell@gmail.com>
2026-05-13 12:10:29 -0500

    works

--- Program.cs
+++ Program.cs
@@ -1,28 +1,28 @@
 ﻿using System.Text.Json;
 using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
 
 namespace Aeoquotes;
 internal class Program
 {
     static string token = File.ReadAllLines("token.txt")[0];
-    static string quoteReactName = "";
-    static string cmdPrefix = "";
     static List<Quote> quotes = [];
-    static Settings settings;
+    public static Settings settings {get; private set; } = new Settings(":joy:", "&");
     public record struct Quote(long id, string nick, ulong userId, string channel, ulong channelId, ulong server, string text, ulong messageId, long unixTime, DateTime dateTime);
-    public record struct Settings(string reactName = "joy", string prefix = "&");
+    public record struct Settings(string reactName = ":joy:", string prefix = "&");
 
     public static void UpdateSettings(Settings newSettings) => UpdateSettings(newSettings.reactName, newSettings.prefix);
 
     public static void UpdateSettings(string reactName, string prefix)
     {
-        quoteReactName = reactName;
-        cmdPrefix = prefix;
-        SaveData(quotes, new Settings(reactName, prefix));
+        settings = new Settings(reactName, prefix);
+        SaveData(quotes, settings);
     }
 
     public static void RemoveQuote(long quoteId)
     {
+
         quotes.RemoveAll(q => q.id == quoteId);
     }
 
@@ -30,35 +30,65 @@ internal class Program
     private static async Task Main(string[] args)
     {
         (quotes, settings) = LoadData();
-        DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(token, DiscordIntents.AllUnprivileged);
+        DiscordClientBuilder builder = DiscordClientBuilder.CreateDefault(token, DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents);
         builder.ConfigureEventHandlers((handler) =>
         {
             handler.HandleMessageReactionAdded(async (client, args) =>
             {
-                if (args.Message.Reactions.Any(react => react.Emoji.Name.Equals(quoteReactName)))
+                Console.WriteLine($"Reaction Added: {args.Emoji.GetDiscordName()}");
+                if (args.Message.Reactions.Any(react => react.Emoji.GetDiscordName().Equals(settings.reactName)))
                 {
                     if (!quotes.Any(q => q.messageId == args.Message.Id)) // if the subset of the quotes list where the message id matches this message is empty, we havent quoted it yet
                     {
-                        quotes.Add(new Quote(
-                            quotes.Count + 1,
-                            args.Message.Author.Username,
-                            args.Message.Author.Id,
-                            args.Channel.Name,
-                            args.Channel.Id,
-                            args.Guild.Id,
-                            args.Message.Content,
-                            args.Message.Id,
-                            args.Message.Timestamp.ToUnixTimeSeconds(),
-                            new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(args.Message.Timestamp.ToUnixTimeSeconds())
-                            )
-                        );
+                        // get the message ourselves because a lot of the fields are null
+                        DiscordChannel channel = await client.GetChannelAsync(args.Channel.Id);
+                        DiscordMessage message = await channel.GetMessageAsync(args.Message.Id);
+                        if (message.Author is not null)
+                        {
+                            DiscordMember author = await channel.Guild.GetMemberAsync(message.Author.Id);
+                                quotes.Add(new Quote(
+                                    quotes.Count + 1,
+                                    author.DisplayName,
+                                    author.Id,
+                                    channel.Name,
+                                    channel.Id,
+                                    channel.Guild.Id,
+                                    message.Content,
+                                    message.Id,
+                                    message.CreationTimestamp.ToUnixTimeSeconds(),
+                                    new DateTime(1970, 1, 1, 0, 0, 0).AddSeconds(message.CreationTimestamp.ToUnixTimeSeconds())
+                                )
+                            );
+                            await message.CreateReactionAsync(
+                                message.Reactions.First(
+                                    react => react.Emoji.GetDiscordName().Equals(settings.reactName)
+                                ).Emoji
+                            );
+                            await channel.SendMessageAsync($"Quote added as #{quotes.Count}");
+                            SaveData(quotes, settings);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Author is null!");
+                        }
+
                     }
                 }
             });
         });
-        DiscordClient discord = builder.Build();
+        builder = builder.UseCommandsNext((cnb) =>
+            {
+                cnb.RegisterCommands<QuoteCommands>();
+            }, 
+            new CommandsNextConfiguration()
+            {
+                StringPrefixes = ["!"]
+            }
+        );
 
+        DiscordClient discord = builder.Build();
         await discord.ConnectAsync();
+        Console.WriteLine("Connected!");
         await Task.Delay(-1);
     }
 
@@ -72,12 +102,33 @@ internal class Program
 
     static (List<Quote>, Settings) LoadData()
     {
-        List<Quote>? quotes = JsonSerializer.Deserialize<List<Quote>>(File.ReadAllText("quotes.json"));
-        Settings settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText("settings.json"));
+        List<Quote>? quotes;
+        Settings newSettings;
+        try
+        {
+            quotes = JsonSerializer.Deserialize<List<Quote>>(File.ReadAllText("quotes.json"));
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            quotes = [];
+        }
+
+        try
+        {
+            newSettings = JsonSerializer.Deserialize<Settings>(File.ReadAllText("settings.json"));
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine(e.Message);
+            newSettings = new Settings(":joy:", "&");
+        }
+
+
         if (quotes is not null)
         {
-            return (quotes, settings);
+            return (quotes, newSettings);
         }
-        else return ([], settings);
+        else return ([], newSettings);
     }
 }
--- QuoteCommands.cs
+++ QuoteCommands.cs
@@ -10,40 +10,88 @@ public class QuoteCommands : BaseCommandModule
     [Command("q")]
     public async Task Q(CommandContext ctx, [RemainingText] string args)
     {
+        Console.WriteLine("q invoked");
         await Quote(ctx, args);
     }
 
     [Command("quote")]
     public async Task Quote(CommandContext ctx, [RemainingText] string args)
     {
+        Console.WriteLine("quote invoked with args:");
+        if (args is null)
+        {
+            args = "";
+        }
         var cmdargs = args.Split(" ");
+        foreach (var item in cmdargs)
+        {
+            Console.Write($" {item} ");
+        }
+        Console.WriteLine();
         // are we asking for a certain quote or a subcommand?
         if (int.TryParse(cmdargs[0], out int id))
         {
+            Console.WriteLine("quoting by number");
             DiscordEmbed quote = await QuoteEmbed(id);
-            await ctx.Channel.SendMessageAsync(quote);
+            if (quote.Title is not null)
+            {
+                await ctx.Channel.SendMessageAsync(quote);
+            }
         } 
         else
         {
             switch (cmdargs[0])
             {
+                case "":
+                    Console.WriteLine("random quote");
+                    DiscordEmbed embed = await RandomQuote();
+                    if (embed.Title is not null)
+                    {
+                        await ctx.Channel.SendMessageAsync(embed);
+                    }
+                    break;
                 case "stats":
-                    DiscordEmbed embed = await QuoteStats();
-                    await ctx.Channel.SendMessageAsync(embed);
+                Console.WriteLine("quoting stats");
+                    DiscordEmbed stats = await QuoteStats();
+                    if (stats.Title is not null)
+                    {
+                        await ctx.Channel.SendMessageAsync(stats);
+                    }
                     break;
                 case "remove":
                 case "delete":
+                    
                     if (int.TryParse(cmdargs[1], out int quoteToRemove))
                     {
-                        RemoveQuote(quoteToRemove);
-                        await ctx.Channel.SendMessageAsync($"Quote {quoteToRemove} removed!");
+                        if (Program.GetQuotes().Count <= quoteToRemove && quoteToRemove > 0)
+                        {
+                            Console.WriteLine($"deleting quote {quoteToRemove}");
+                            // need to remove our reaction
+                            var message = await ctx.Channel.GetMessageAsync(Program.GetQuotes()[quoteToRemove - 1].messageId);
+                            var user = await ctx.Guild.GetMemberAsync(1503994723118088292);
+                            await message.DeleteReactionAsync(message.Reactions.First(r => r.Emoji.GetDiscordName() == Program.settings.reactName).Emoji, user);
+                            RemoveQuote(quoteToRemove);
+                            
+                            await ctx.Channel.SendMessageAsync($"Quote {quoteToRemove} removed!");
+                        }
+                        else
+                        {
+                            await ctx.Channel.SendMessageAsync($"Quote {quoteToRemove} not found");
+                        }
+
                     }
                     break;
                 default:
                     // assume arg is a username, so get their id
-                    var targetUserId = ctx.Guild.Members.First(m => m.Value.Nickname.ToLowerInvariant().Equals(cmdargs[0]) || m.Value.Username.ToLowerInvariant().Equals(cmdargs[0])).Key;
+                    Console.WriteLine("quoting by username");
+                    var targetUserId = ctx.Guild.Members.First(m => m.Value.DisplayName.ToLowerInvariant().Equals(cmdargs[0]) || m.Value.Username.ToLowerInvariant().Equals(cmdargs[0])).Key;
                     long quoteId = await UsernameQuote(targetUserId);
-                    await QuoteEmbed(quoteId);
+                    Console.WriteLine(quoteId);
+                    DiscordEmbed usernameQuote = await QuoteEmbed(quoteId);
+                    if (usernameQuote.Title is not null)
+                    {
+                        await ctx.Channel.SendMessageAsync(usernameQuote);
+                    }
                     break;
             }  
         }
@@ -68,17 +116,26 @@ public class QuoteCommands : BaseCommandModule
 
     static async Task<DiscordEmbed> QuoteEmbed(long id)
     {
-        // assemble the top 20
-        Program.Quote quote = Program.GetQuotes().Find(q => q.id == id);
-        DiscordEmbedBuilder embedBuilder = new();
-        StringBuilder listBuilder = new();
-        embedBuilder.Title = $"#{quote.id}";
-        StringBuilder descBuilder = new();
-        descBuilder.Append(quote.text + "\n");
-        descBuilder.Append($"* <@{quote.userId}> [(Jump)](https://discordapp.com/channels/{quote.server}/{quote.channelId}/{quote.messageId})");
-        embedBuilder.Description = descBuilder.ToString();
-        embedBuilder.WithTimestamp(quote.dateTime);
-        return embedBuilder.Build();
+        if (id > 0)
+        {
+            Program.Quote quote = Program.GetQuotes().Find(q => q.id == id);
+            DiscordEmbedBuilder embedBuilder = new();
+            StringBuilder listBuilder = new();
+            embedBuilder.Title = $"#{quote.id}";
+            StringBuilder descBuilder = new();
+            descBuilder.Append(quote.text + "\n");
+            descBuilder.Append($"* <@{quote.userId}> [(Jump)](https://discordapp.com/channels/{quote.server}/{quote.channelId}/{quote.messageId})");
+            embedBuilder.Description = descBuilder.ToString();
+            embedBuilder.WithTimestamp(quote.dateTime);
+            return embedBuilder.Build();
+        }
+        else
+        {
+            return new DiscordEmbedBuilder()
+            {
+                Title = null
+            }.Build();
+        }
     }
 
     static async Task<long> UsernameQuote(ulong userid)
@@ -92,11 +149,23 @@ public class QuoteCommands : BaseCommandModule
         }
         // they have quotes, so pick a random one
         Random rng = new(DateTime.Now.Microsecond);
-        var quoteIndex = rng.NextInt64(quotesByUser.Count() + 1);
+        var quoteIndex = rng.NextInt64(quotesByUser.Count());
         // get id of this quote
         // first sort by id
         var sortedQuotes = quotesByUser.OrderBy(q => q.id).ToList();
         // then pull the quote id
         return sortedQuotes[(int)quoteIndex].id;
+    }
+
+    static async Task<DiscordEmbed> RandomQuote()
+    {
+        Random rng = new(DateTime.Now.Microsecond);
+        var quotes = Program.GetQuotes();
+        if (quotes.Count > 0)
+        {
+            long id = rng.NextInt64(quotes.Max(q => q.id) + 1);
+            return await QuoteEmbed(id);
+        }
+        return await QuoteEmbed(-1);
     }
 }
